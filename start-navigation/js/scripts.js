0c80276cd67847a85a6391fb2fac782301b57272
dic09005 <dic09005@byui.edu>
2022-10-05 20:40:17 -0600

    commit

---
+++ start-navigation/js/scripts.js
@@ -0,0 +1,18 @@
+
+hamburgerBtn
+primaryNav
+
+
+function toggleMenu() {
+
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn')
+
+x.onclick = toggleMenu;
+
+
+
+
