0089b127b6cc9dfd03c9e7545a9d0a027f097c3d
dic09005 <dic09005@byui.edu>
2022-10-06 22:56:30 -0600

    commit

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -13,4 +13,13 @@ const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
 
+const datefield = document.querySelector(".date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
 
