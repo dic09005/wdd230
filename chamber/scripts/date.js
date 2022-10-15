9542cd29c80b4c982797bdfdc7bcb0ff30386595
dic09005 <dic09005@byui.edu>
2022-10-14 22:10:11 -0600

    commit

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -2,16 +2,6 @@ date = new Date().getFullYear();
 document.querySelector("#year").innerHTML = date
 document.querySelector("#timestamp").innerHTML = document.lastModified;
 
-function toggleMenu() {
-
-    document.getElementById('primaryNav').classList.toggle('open');
-    document.getElementById('hamburgerBtn').classList.toggle('open');
-}
-
-const x = document.getElementById('hamburgerBtn')
-
-x.onclick = toggleMenu;
-
 
 const datefield = document.querySelector(".date");
 
