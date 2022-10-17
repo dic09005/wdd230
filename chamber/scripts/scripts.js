9c8bcbf7f1869a59a9d0fda75a0352b19602172a
dic09005 <dic09005@byui.edu>
2022-10-17 10:28:24 -0600

    commit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -8,9 +8,7 @@ const x = document.getElementById('hamburgerBtn')
 
 x.onclick = toggleMenu;
 
-let memo = date.getDay();
-if (memo == 1 || memo ==2) {
-    let banner = document.getElementById("banner");
-    banner.style.display = "block";
-    banner.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+let day = date.getDay();
+if (day == 1 || day ==2) {
+    document.getElementById('banner').innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
