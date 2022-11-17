cc9a7e220906a3b3b3be321e031e13b81e823cdb
dic09005 <dic09005@byui.edu>
2022-11-16 21:34:29 -0700

    commit weather

---
+++ lesson10/chamber/scripts/scripts.js
@@ -0,0 +1,77 @@
+
+//Text banner
+/*
+let day = bdate.getDay();
+if (day == 1 || day ==2) {
+    let banner = document.getElementById("banner");
+    if (banner != undefined) {
+        banner.style.display = "block";
+        banner.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    }
+}
+*/
+
+// Date header and footer
+
+date = new Date().getFullYear();
+document.querySelector("#year").innerHTML = date
+document.querySelector("#timestamp").innerHTML = document.lastModified;
+
+function toggleMenu() {
+
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn')
+
+x.onclick = toggleMenu;
+
+
+const datefield = document.querySelector(".date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// lazyload
+// Brother Blazzard's codepin
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const ingOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    };
+  };
+
+
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+      
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
---
+++ lesson10/chamber/scripts/weatherapi.js
@@ -0,0 +1,66 @@
+//Most of this is from Brother Blazzards Lab windchill formula is from dev.to Chris Bongers
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windChill = document.querySelector('#windchill');
+const windSpeed = document.querySelector('#windspeed');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Rigby&appid=f6cd64304c91eee8c1c9d4ad1012ca4d&units=imperial';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); 
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+// capitalize function for weather condition
+
+function capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+}
+
+// Display the Results
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = capitalize(desc);
+
+    temp = currentTemp
+    // console.log(temp)
+
+    ws = windSpeed
+    // console.log(ws)
+
+    let windchill = "";
+
+    if (temp <= 50 & ws > 3) {
+        windchill = Math.round(35.74 + (0.6215 * temp) -
+            (35.75 * (ws ** 0.16)) +
+            (0.4275 * temp * (ws ** 0.16)));
+        }          
+    else {
+        windchill = "N/A";
+    }
+    windChill.textContent = `${windchill} °F`;
+    //console.log(windchill)
+}
