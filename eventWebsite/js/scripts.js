0613ca3fc38a86ff2d3563147e3d4fe93596c6ab
Rob Dickson <97182224+dic09005@users.noreply.github.com>
2023-04-05 12:25:55 -0600

    eventSite

---
+++ eventWebsite/js/scripts.js
@@ -0,0 +1,9 @@
+// Scripts to toggle Menu Button
+
+function toggleMenu() {
+
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+
